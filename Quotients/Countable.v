54ec9649379762c5bdff471937a09705c5c1919a
Arnaud Spiwack <arnaud@spiwack.net>
2014-12-24 12:37:37 +0100

    More results on countability.

--- Quotients/Countable.v
+++ Quotients/Countable.v
@@ -10,4 +10,51 @@
     A type [A] is called countable if [option A] is a retract of the
     natural numbers. When [A] is inhabited, then it is equivalent to a
     retraction of [A] into the natural numbers, but this definition
-    also includes the empty set. *)
+    also includes the empty set.
+
+    Being countable is defined here as a type class, making the
+    hypothesis that the chosen retraction is unlikely to be tractable
+    (few of the instances defined in this file are) and that it's not
+    to be used in a real program, where some control will be
+    needed. *)
+
+Require Import FinSet.Quotients.Retract.
+Require Import Coq.NArith.NArith.
+
+
+Class Countable A := countable : Retract nat (option A).
+
+(** We also use an inhabitation type class, which will help avoid the
+    [option] type in our calculation. *)
+Class Inhabited A := inhabitant : A.
+
+Definition to_nat {A} {_:Countable A} {_:Inhabited A} : A -> nat :=
+  (retract_nat_inh countable inhabitant).(inj).
+Definition of_nat {A} {_:Countable A} {_:Inhabited A} : nat -> A :=
+  (retract_nat_inh countable inhabitant).(proj).
+
+Lemma nat_retract A {_:Countable A} {_:Inhabited A} : forall x, of_nat (to_nat x) = x.
+Proof.
+  intros *. unfold of_nat, to_nat.
+  now rewrite !retract.
+Qed.
+
+(** The typical way of proving that a type is countable is to exhibit
+    a retraction towards a known countable type. *)
+Program Definition countable_of_retract {A B} {_:Countable A} (r:Retract A B) : Countable B :=
+  Retract.compose countable (Retract.opt_lift r).
+
+
+(** * Countable instances *)
+
+Instance countable_nat : Countable nat := retract_nat_opt Retract.id.
+Instance countable_n   : Countable N := retract_nat_opt retract_nat_N.
+
+Program Instance countable_empty_set : Countable Empty_set := {|
+  inj x := 0;
+  proj x := None
+|}.
+Next Obligation.
+  intros [ [] | ].
+  reflexivity.
+Qed.
--- Quotients/Retract.v
+++ Quotients/Retract.v
@@ -1,8 +1,8 @@
 (** This file defines retractions and basic properties on them. *)
 
 Require Coq.Program.Program.
-
-Obligation Tactic := try solve [intuition eauto].
+Require Coq.NArith.NArith.
+Require Coq.Lists.List.
 
 
 (** [Retract A B] is the type of retractions from [B] to [A]. *)
@@ -13,6 +13,8 @@ Record Retract A B := {
 }.
 Arguments inj  {A B} _ _.
 Arguments proj {A B} _ _.
+
+Obligation Tactic := try solve [intuition eauto|intros;now rewrite !retract].
 
 (** * Categorical combinators *)
 
@@ -26,7 +28,6 @@ Program Definition compose {A B C}
   inj  x := f.(inj)  (g.(inj)  x);
   proj x := g.(proj) (f.(proj) x)
 |}.
-Next Obligation. intros **. now rewrite !retract. Qed.
 
 Program Definition opt_compose {A B C}
      (f:Retract A (option B)) (g:Retract B (option C))
@@ -37,7 +38,22 @@ Program Definition opt_compose {A B C}
             | None   => None
             end
 |}.
-Next Obligation. intros **. now rewrite !retract. Qed.
+
+Program Definition opt_lift {A B}
+     (f:Retract A B) : Retract (option A) (option B) := {|
+  inj  x := match x return _ with
+            | Some x => Some (f.(inj) x)
+            | None   => None
+            end;
+  proj x := match x return _ with
+            | Some x => Some (f.(proj) x)
+            | None   => None
+            end
+|}.
+Next Obligation.
+  intros A B f [x|].
+  all: now rewrite ?retract.
+Qed.
 
 (** * Retracts of the natural numbers *)
 
@@ -62,3 +78,100 @@ Next Obligation.
   + now rewrite !retract.
   + reflexivity.
 Qed.
+
+(** A retraction from [A+1] into [nat] restricts to a retraction from
+    [A] to [nat] when [A] is inhabited. *)
+Program Definition retract_nat_inh {A}
+     (f:Retract nat (option A)) (a:A) : Retract nat A := {|
+  inj  x := f.(inj) (Some x) ;
+  proj x := match f.(proj) x return _ with
+            | Some x' => x'
+            | None    => a
+            end
+|}.
+
+
+(** * Standard retractions *)
+
+Import NArith.
+
+Definition retract_nat_N : Retract nat N := {|
+  inj  := N.to_nat ;
+  proj := N.of_nat ;
+  retract := N2Nat.id
+|}.
+
+
+(** In this module we establish that [list nat] is a retract of
+    [N]. We use, to that effect the following Gӧdel numbering:
+    encoding [[n₁;...;nₖ]] as [[0...n₁...01...10...nₖ...01]] (least
+    significant bit to the left).
+
+    Examples:
+    - [[]] is encoded as [0].
+    - [[0]] is encoded as [1].
+    - [[2]] is encoded as [001] (4).
+    - [[3;2]] is encoded as [0001001] (72). *)
+
+Module RetractNListNat.
+
+ Import NArith List List.ListNotations.
+
+ Local Open Scope positive.
+ Local Open Scope N.
+
+ (** [encode_nat n p] is [p] preceeded by [n] [0]-s. *)
+ Fixpoint encode_nat (n:nat) (p:N) : N :=
+   match n with
+   | 0%nat => p
+   | S n => N.double (encode_nat n p)
+   end
+ .
+
+ Definition encode : list nat -> N :=
+   List.fold_right (fun n p => encode_nat n (N.succ_double p)) 0.
+
+ (** Positive integers decode to non-empty lists. *)
+ Fixpoint decode_positive (p:positive) : nat * list nat :=
+   match p with
+   | 1%positive => (0%nat,[])
+   | p~0 => let (n,l) := decode_positive p in (S n, l)
+   | p~1 => let (n,l) := decode_positive p in (0%nat,n::l)
+   end
+ .
+
+ Definition decode (n:N) : list nat :=
+   match n with
+   | 0 => []
+   | Npos p => let (n,l) := decode_positive p in n::l
+ end.
+
+ Lemma retract : forall l, decode (encode l) = l.
+ Proof.
+   assert (forall n, decode (N.succ_double n) = 0%nat::(decode n)) as decode_succ_double.
+   { intros [ | p ].
+     + reflexivity.
+     + cbn.
+       now destruct (decode_positive p). }
+   intros l.
+   induction l as [ | n l hl ].
+   + reflexivity.
+   + cbn.
+     induction n as [ | n hn ].
+     * cbn. rewrite decode_succ_double.
+       now rewrite hl.
+     * cbn.
+       unfold decode in hn |- *.
+       destruct (encode_nat n (N.succ_double (encode l))); cbn.
+       - easy.
+       - destruct (decode_positive p).
+         congruence.
+ Qed.
+
+End RetractNListNat.
+
+Definition retract_N_list_nat : Retract N (list nat) := {|
+  inj  := RetractNListNat.encode ;
+  proj := RetractNListNat.decode ;
+  retract := RetractNListNat.retract
+|}.
